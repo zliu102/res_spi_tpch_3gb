7fc2cd407a90bab43ff3d484f46df38a5b8490a4
zliu102 <zliu102@hawk.iit.edu>
2023-05-09 02:16:13 -0500

    a

--- res_spi_tpch_3gb.c
+++ res_spi_tpch_3gb.c
@@ -322,11 +322,15 @@ reservoir_sampler_tpch_3gb(PG_FUNCTION_ARGS)
 
 
     //tupdesc = SPI_tuptable->tupdesc;
-    tupdesc = CreateTemplateTupleDesc(4, false);
+    tupdesc = CreateTemplateTupleDesc(8, false);
     TupleDescInitEntry(tupdesc, (AttrNumber) 1, "l_suppkey", INT4OID, -1, 0);
     TupleDescInitEntry(tupdesc, (AttrNumber) 1, "l_discount", NUMERICOID, -1, 0);
     TupleDescInitEntry(tupdesc, (AttrNumber) 1, "l_tax", NUMERICOID, -1, 0);
     TupleDescInitEntry(tupdesc, (AttrNumber) 1, "l_extendedprice", INT4OID, -1, 0);
+    TupleDescInitEntry(tupdesc, (AttrNumber) 1, "l_quantity", INT4OID, -1, 0);
+    TupleDescInitEntry(tupdesc, (AttrNumber) 1, "l_orderkey", INT4OID, -1, 0);
+    TupleDescInitEntry(tupdesc, (AttrNumber) 1, "l_partkey", INT4OID, -1, 0);
+    TupleDescInitEntry(tupdesc, (AttrNumber) 1, "l_linenumber", INT4OID, -1, 0);
     oldcontext = MemoryContextSwitchTo(rsinfo->econtext->ecxt_per_query_memory);
     tupstore = tuplestore_begin_heap(true, false, work_mem);
     rsinfo->setResult = tupstore;
